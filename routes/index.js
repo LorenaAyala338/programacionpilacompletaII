0e47a335f81b491611ac7a3ca599fda6cb2d860f
LorenaAyala338 <lorennqqyala@gmail.com>
2021-04-07 20:23:22 -0500

    cambio copañia

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', author: 'Lorena Ayala', appName: 'WebApp' });
+  res.render('index', { title: 'Express', author: 'Lorena Ayala', appName: 'WebApp',escuela:'ITGAM' });
 });
 
 /* Agregar Ruta */
